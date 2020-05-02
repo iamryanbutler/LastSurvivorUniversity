667d68805cb540de64302fd5a7fd010c7ff991d5
Jacob Swanson <jswan12@users.noreply.github.com>
2020-05-01 21:47:49 -0500

    Make much needed updates, fix dialogue bugs, reposition camera

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public List<string> messages;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (this.enabled && other.CompareTag("Player"))
+        {
+            DialogueManager.instance.PushDialogue(messages);
+            this.enabled = false;
+        }
+    }
+}
